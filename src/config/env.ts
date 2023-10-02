4bda38025d046150f36a42df8a90395ea849e413
Mondo <mondo.jiang@wisc.edu>
2023-10-01 19:29:48 -0500

    feat(connection): add local env's connection, fix posibble permission error with local mount

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 export const PORT = process.env.PORT || '8000';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = parseInt(process.env.DB_PORT || '3306', 10);
--- src/connection.ts
+++ src/connection.ts
@@ -7,6 +7,7 @@ import {
   DB_USERNAME,
 } from './config/env';
 import { VError } from 'verror';
+import { appLogger } from './logger';
 
 /**
  * Create connection to database.
@@ -24,6 +25,7 @@ export async function createConnection() {
     });
 
     const connection = await ds.initialize();
+    appLogger.info('Database connection established');
     return connection;
   } catch (err) {
     throw new VError(err, 'error connecting to database');
